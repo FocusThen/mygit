df936d259722cc90ba1f32af9cf96e0e5c224e2d
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-03-14 07:03:09 +0000

    feat: base init

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ build:
 	@go build -o bin/mygit cmd/mygit/main.go
 
 run: build
-	@./bin/mygit
+	@./bin/mygit $(var)
 
--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello %s", "World!")
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
+		os.Exit(1)
+	}
+
+	switch command := os.Args[1]; command {
+	case "init":
+		for _, dir := range []string{".mygit", ".mygit/objects", ".mygit/refs"} {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+		}
+
+		headFileContents := []byte("ref: refs/heads/main\n")
+		if err := os.WriteFile(".mygit/HEAD", headFileContents, 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+		}
+		fmt.Println("Initialized mygit directory")
+
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		os.Exit(1)
+	}
 }
